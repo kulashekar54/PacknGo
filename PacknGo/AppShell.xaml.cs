cdd537bb4518590b01852faf1f5033a6c212314e
Kulashekar <ksengan@thapovan-inc.com>
2025-07-22 10:58:29 +0530

    Basic Setup : .NET 9 Project Creation

---
+++ PacknGo/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace PacknGo;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ PacknGo/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace PacknGo;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
